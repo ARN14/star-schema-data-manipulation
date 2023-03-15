6abbb6188aa6f11c7c273ca6d9ec8b05ef11ca13
ARN14 <ar14@outlook.com>
2023-03-15 12:54:24 +0300

    Fixed pep8 issues

--- src/format_stock.py
+++ src/format_stock.py
@@ -1,2 +1,5 @@
 def format_stock(input_list):
-    return [[item["item_name"], item["amount"]] for item in input_list if "item_name" in item and "amount" in item]
+    return [[item["item_name"], item["amount"]] for item in input_list
+            if "item_name" in item
+            and item["item_name"]
+            and "amount" in item]
