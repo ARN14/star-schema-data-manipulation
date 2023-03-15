dc53e8500c8f8640cc056fd9bc7a38c67455f0f1
ARN14 <ar14@outlook.com>
2023-03-15 17:43:00 +0300

    Fix Pep8 issues

--- src/format_staff.py
+++ src/format_staff.py
@@ -2,16 +2,17 @@ def format_staff(staff_data, department_data):
     formatted_staff = []
 
     for staff in staff_data:
-        department_id = [row[0] for row in department_data if row[1] == staff["department"]]
+        department_id = [row[0] for row in department_data
+                         if row[1] == staff["department"]]
 
         if not department_id:
             continue
 
         formatted_staff.append(
-                [
-                    staff["first_name"],
-                    staff["last_name"],
-                    department_id[0]
-                ])
+            [
+                staff["first_name"],
+                staff["last_name"],
+                department_id[0]
+            ])
 
     return formatted_staff
--- src/format_stock_feature.py
+++ src/format_stock_feature.py
@@ -2,8 +2,11 @@ def format_stock_feature(stock, features, original_data):
     stocks_features = []
 
     for item in original_data:
-        feature_ids = [column[0] for column in features if column[1] in item["features"]]
-        stock_id = [column[0] for column in stock if column[1] == item["item_name"]]
+        feature_ids = [column[0] for column in features
+                       if column[1] in item["features"]]
+        stock_id = [column[0]
+                    for column in stock
+                    if column[1] == item["item_name"]]
 
         for feature in feature_ids:
             stocks_features.append([feature, stock_id[0]])
