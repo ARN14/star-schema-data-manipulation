30da99167ffa4c0d670feb9737721489a788fc14
ARN14 <ar14@outlook.com>
2023-03-15 12:54:05 +0300

    Fixed pep8 issues

--- src/format_departments.py
+++ src/format_departments.py
@@ -1,8 +1,12 @@
-def format_departments(input_list):    
+def format_departments(input_list):
     departments_list = []
 
     for dictionary in input_list:
-        if "department" in dictionary and dictionary["department"] not in departments_list:
+        if "department" in dictionary\
+                and dictionary["department"]\
+                and type(dictionary["department"]) == str\
+                and dictionary["department"] not in departments_list:
+
             departments_list.append(dictionary["department"])
 
     return [[department] for department in departments_list]
